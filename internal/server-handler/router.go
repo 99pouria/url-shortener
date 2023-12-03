81f26a472b776b46ba355aa2a49a50a712097bc1
Pooria Kalantari <46835187+pooria1@users.noreply.github.com>
2023-12-04 02:29:30 +0330

    [WIP] fix server bugs

--- internal/server-handler/router.go
+++ internal/server-handler/router.go
@@ -3,6 +3,8 @@ package server_handler
 import (
 	"fmt"
 	"net/http"
+	"net/url"
+	"sync/atomic"
 
 	"github.com/99pouria/url-shortener/internal/config"
 	"github.com/99pouria/url-shortener/internal/database"
@@ -11,19 +13,29 @@ import (
 )
 
 type Server struct {
-	e  *echo.Echo
-	db database.DatabaseURL
-	sh *shortener.Shortener
+	echoInstance *echo.Echo
+	db           database.DatabaseURL
+	shortenerAPI *shortener.Shortener
+	address      string      // address of server
+	isStart      atomic.Bool // isStart shows if server is running or not
 }
 
-func NewServer() (Server, error) {
+// NewServer creates new connection to redis database and starts serving address (which has been set in
+// config file).
+//
+// This server accepts a POST method with 'longURL' form value and returns a short URL that if you call
+// the URL it redirects to original URL (long URL).
+func NewServer() (*Server, error) {
 	var (
 		server Server
 		cfg    = config.GetConfig()
 	)
 
+	server.address = fmt.Sprintf("%s:%d", cfg.ServerConfig.Hostname, cfg.ServerConfig.Port)
+	server.isStart.Store(false)
+
 	// echo
-	server.e = echo.New()
+	server.echoInstance = echo.New()
 
 	// database
 	server.db = database.NewClient(
@@ -34,56 +46,59 @@ func NewServer() (Server, error) {
 
 	keys, err := server.db.KeyCount()
 	if err != nil {
-		fmt.Println("e")
-		return Server{}, err
+		return nil, err
 	}
 
 	// shortener
-	server.sh = shortener.NewShortener(
-		fmt.Sprintf(
-			"%s:%d",
-			cfg.ServerConfig.Hostname,
-			cfg.ServerConfig.Port,
-		),
-		keys,
-	)
-	return server, nil
+	server.shortenerAPI = shortener.NewShortener(keys)
+	return &server, nil
 }
 
-func (s Server) Run() {
+// Run starts serving clients
+//
+// Do not forget to defer 'Close' function
+func (s *Server) Run() error {
+	if !s.isStart.CompareAndSwap(false, true) {
+		return fmt.Errorf("server is already running")
+	}
+	defer s.isStart.Store(false)
 
-	e := echo.New()
-
-	e.GET("/:key", s.open)
-	e.POST("/create", s.create)
-
-	e.Logger.Fatal(e.Start(":8080"))
+	s.echoInstance.GET("/:key", s.open)
+	s.echoInstance.POST("/create", s.create)
+	return s.echoInstance.Start(s.address)
 }
 
-func (s Server) open(c echo.Context) error {
+// open redirects given url to original URL
+func (s *Server) open(c echo.Context) error {
 	longURL, err := s.db.Get(c.Param("key"))
 	if err != nil {
-		return c.String(http.StatusOK, fmt.Sprintf("can't retrieve main URL: %s", err.Error()))
+		return c.String(http.StatusBadRequest, fmt.Sprintf("original URL not found: %s", err.Error()))
 	}
+
 	return c.Redirect(http.StatusMovedPermanently, longURL)
 }
 
-func (s Server) create(c echo.Context) error {
-	longURL := c.FormValue("longURL")
-
-	key, err := s.sh.GenerateNewKey()
+// create maps original URL to a short and unique URL and sends short URL as response
+func (s *Server) create(c echo.Context) error {
+	key, err := s.shortenerAPI.GenerateNewKey()
 	if err != nil {
-		c.String(http.StatusOK, fmt.Sprintf("can't generate new key: %s", err.Error()))
+		c.String(http.StatusInternalServerError, fmt.Sprintf("can't generate new URL: %s", err.Error()))
 	}
 
-	s.db.Add(key, longURL)
+	s.db.Add(key, c.FormValue("longURL"))
 	if err != nil {
-		c.String(http.StatusOK, fmt.Sprintf("can't generate new key: %s", err.Error()))
+		c.String(http.StatusInternalServerError, fmt.Sprintf("can't add short URL to database: %s", err.Error()))
 	}
-	return c.String(http.StatusOK, key)
+
+	shortURL, err := url.JoinPath("https://", s.address, key)
+	if err != nil {
+		c.String(http.StatusInternalServerError, fmt.Sprintf("can't create short url: %s", err.Error()))
+	}
+	return c.String(http.StatusOK, shortURL)
 }
 
-func (s Server) Close() {
+// Close closes the connection to database and http serving connection
+func (s *Server) Close() {
 	s.db.Close()
-	s.e.Close()
+	s.echoInstance.Close()
 }
