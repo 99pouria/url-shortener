81f4cd62810283ba6a0c61234aa4667d3a7acc47
Pooria Kalantari <46835187+pooria1@users.noreply.github.com>
2023-12-04 02:28:37 +0330

    defer handlers

--- cmd/url_shortener/main.go
+++ cmd/url_shortener/main.go
@@ -12,31 +12,40 @@ import (
 )
 
 func main() {
+	// handling ctrl+c and SIGKILL signal
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, unix.SIGKILL)
 	go func() {
 		for sig := range c {
 			fmt.Printf("signal %s received. closing...\n", sig.String())
 			handle()
+			os.Exit(0)
 		}
 	}()
+	defer handle()
 
 	// loading config content
 	if err := config.LoadConfig(); err != nil {
 		log.Fatal(err)
 	}
 
+	// creating new server instance
 	s, err := server_handler.NewServer()
 	if err != nil {
 		log.Fatal(err)
 	}
-	handlers = append(handlers, s.Close)
 
-	s.Run()
+	// lets run!
+	if err := s.Run(); err != nil {
+		log.Fatal(err)
+	}
+	handlers = append(handlers, s.Close) // add close function of server to handlers
 }
 
+// handlers containts all handler which must call before finishing process
 var handlers []func()
 
+// handle calls all handler functions
 func handle() {
 	for _, handle := range handlers {
 		handle()
