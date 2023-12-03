b0f6308b2482ee93350e33e9bc6f07e77b04efa8
Pooria Kalantari <46835187+pooria1@users.noreply.github.com>
2023-12-04 02:29:41 +0330

    fix shortener bugs

--- internal/shortener/shortener.go
+++ internal/shortener/shortener.go
@@ -1,8 +1,6 @@
 package shortener
 
 import (
-	"fmt"
-	"net/url"
 	"sync/atomic"
 
 	"github.com/99pouria/url-shortener/internal/shortener/converter"
@@ -11,30 +9,21 @@ import (
 // Shortener keeps instance of url-shortener
 type Shortener struct {
 	currentStep atomic.Uint32
-	hostname    string
 }
 
 // NewShortener creates new 'Shortener' pointer which starts to generate short urls
 // by converting given step to a number based 62.
 //
-// Negative start step replaces with zero and there is no validation for hostname.
-// Make sure you are giving appropriate one.
-func NewShortener(hostname string, startStep int) *Shortener {
+// Negative start step replaces with zero.
+func NewShortener(startStep int) *Shortener {
 	if startStep < 0 {
 		startStep = 0
 	}
 
-	s := Shortener{hostname: hostname}
+	s := Shortener{}
 	s.currentStep.Store(uint32(startStep))
 	return &s
 }
 
-// GenerateNewKey
-func (s *Shortener) GenerateNewKey() (string, error) {
-	query, err := converter.ItoA(s.currentStep.Add(1))
-	if err != nil {
-		return "", err
-	}
-	fmt.Println(query)
-	return url.JoinPath(s.hostname, query)
-}
+// GenerateNewKey retuns unique key
+func (s *Shortener) GenerateNewKey() (string, error) { return converter.ItoA(s.currentStep.Add(1)) }
